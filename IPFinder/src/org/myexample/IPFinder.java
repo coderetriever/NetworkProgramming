889bc46ebde88474bdf9396da63267054993c4d5
codeblocks2 <code.blocks.user2@gmail.com>
2014-07-14 23:56:22 -0400

    main comment

--- IPFinder/src/org/myexample/IPFinder.java
+++ IPFinder/src/org/myexample/IPFinder.java
@@ -4,6 +4,7 @@ import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
+//Finds the IP address of the localhost and a specified host
 public class IPFinder 
 {
 	public static void main(String[] args)
