b643067efc943440207366eedf36e5ed2ffb95ce
coderetrieverblah <code.retriever.blah@gmail.com>
2014-07-14 20:51:31 -0400

    Enhanced code

--- IPFinder/src/org/myexample/IPFinder.java
+++ IPFinder/src/org/myexample/IPFinder.java
@@ -1,5 +1,28 @@
 package org.myexample;
 
-public class IPFinder {
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
 
+public class IPFinder 
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			//Get address of local host
+			InetAddress address = InetAddress.getLocalHost();			
+			System.out.println("Localhost is: " + address.toString());
+			
+			//Get address of a specified host
+			Scanner input = new Scanner(System.in);
+			System.out.print("Enter the hostname:");
+			String host = input.next();
+			
+			System.out.println("Address of " + host + " is:" + InetAddress.getByName(host));
+		}catch(UnknownHostException e)
+		{
+			
+		}
+	}
 }
